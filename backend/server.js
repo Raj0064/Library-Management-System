e752dc3edd7335ecbf4248c0cbc5c05e8a4e9e0e
Raj0064 <nandanrajdeep2022@gmail.com>
2025-01-21 02:18:52 +0530

    build

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import adminRoute from './routes/adminRoute.js';
 import bookRoute from './routes/bookRoute.js';
 import cors from 'cors';
 import connectDB from './utils/db.js';
+import path from 'path';
+
+
 dotenv.config();
 const app=express();
 
@@ -13,6 +16,9 @@ const app=express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
+const _dirname=path.resolve();
+
 
 const corsOption = {
   origin: "http://localhost:5173", // Specify the client origin
@@ -25,6 +31,10 @@ app.use("/api/v1/user",userRoute);
 app.use("/api/v1/admin",adminRoute);
 app.use("/api/v1/book", bookRoute);
 
+app.use(express.static(path.join(_dirname, '/frontend/dist')));
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(_dirname, 'frontend', 'dist', 'index.html'))  
+);
 
 const PORT=process.env.PORT||8000;
 app.listen(PORT,()=>{
