b80e5c745730014e4c2624e8ee1fc296a976f5c7
Raj0064 <nandanrajdeep2022@gmail.com>
2025-01-21 03:44:13 +0530

    New url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -4,6 +4,6 @@
 export const USER_API_END_POINT =
   "https://library-management-system-oo5a-892t8i5ev.vercel.app/api/v1/user";
 export const ADMIN_API_END_POINT =
-  "https://library-management-system-oo5a-892t8i5ev.vercel.app/api/v1/admin";
+  "https://library-management-system-oo5a-892t8i5ev.vercel.app/api/v1/admin" ;
 export const BOOK_API_END_POINT =
   "https://library-management-system-oo5a-892t8i5ev.vercel.app/api/v1/book";
