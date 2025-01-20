31f6d542f3fbebad0eb005aa5fcfadb2bf8d6177
Raj0064 <nandanrajdeep2022@gmail.com>
2025-01-21 03:55:27 +0530

    Render url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -2,8 +2,8 @@
 // export const ADMIN_API_END_POINT = "http://localhost:3000/api/v1/admin";
 // export const BOOK_API_END_POINT = "http://localhost:3000/api/v1/book";
 export const USER_API_END_POINT =
-  "https://library-management-system-oo5a-892t8i5ev.vercel.app/api/v1/user";
+  "https://library-management-system-yhmk.onrender.com/api/v1/user";
 export const ADMIN_API_END_POINT =
-  "https://library-management-system-oo5a-892t8i5ev.vercel.app/api/v1/admin" ;
+  "https://library-management-system-yhmk.onrender.com/api/v1/admin"; ;
 export const BOOK_API_END_POINT =
-  "https://library-management-system-oo5a-892t8i5ev.vercel.app/api/v1/book";
+  "https://library-management-system-yhmk.onrender.com/api/v1/book";
